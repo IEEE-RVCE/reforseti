3fee82439c04942a1ff9487df38e3b3f4a358230
Atreya Bain <atreyabain@gmail.com>
2023-10-22 21:59:13 +0530

    [add] controller test

--- src/main/java/org/ieeervce/api/siterearnouveau/controller/ArticlesController.java
+++ src/main/java/org/ieeervce/api/siterearnouveau/controller/ArticlesController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.ieeervce.api.siterearnouveau.dto.ResultsDTO;
 import org.ieeervce.api.siterearnouveau.entity.Article;
-import org.ieeervce.api.siterearnouveau.repository.ArticlesRepository;
 import org.ieeervce.api.siterearnouveau.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
