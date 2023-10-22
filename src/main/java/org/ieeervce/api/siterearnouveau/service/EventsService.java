9ac1e276b4ceda19dde3a5c09b9299f511a43839
Prajwal P <prajwalprakash3722@gmail.com>
2023-10-22 18:50:53 +0530

    fix at EventService

--- src/main/java/org/ieeervce/api/siterearnouveau/service/EventsService.java
+++ src/main/java/org/ieeervce/api/siterearnouveau/service/EventsService.java
@@ -2,18 +2,17 @@ package org.ieeervce.api.siterearnouveau.service;
 
 import java.util.List;
 
-import org.ieeervce.api.siterearnouveau.entity.Article;
-import org.ieeervce.api.siterearnouveau.repository.ArticlesRepository;
+import org.ieeervce.api.siterearnouveau.entity.Event;
+import org.ieeervce.api.siterearnouveau.repository.EventsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EventsService {
     @Autowired
-    ArticlesRepository articlesRepository;
+    EventsRepository eventsRepository;
 
-
-    public List<Article> list(){
-        return articlesRepository.findAll();
+    public List<Event> list() {
+        return eventsRepository.findAll();
     }
 }
