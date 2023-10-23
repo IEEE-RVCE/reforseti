0916ca22661a1acf60f9c4e24f6e81fd9553a4bc
Prajwal P <prajwalprakash3722@gmail.com>
2023-10-24 00:39:37 +0530

    [fix] stupid merge conflict

--- src/test/java/org/ieeervce/api/siterearnouveau/controller/ArticlesControllerTest.java
+++ src/test/java/org/ieeervce/api/siterearnouveau/controller/ArticlesControllerTest.java
@@ -22,8 +22,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import jakarta.persistence.criteria.CriteriaBuilder.In;
-
 @ExtendWith(MockitoExtension.class)
 class ArticlesControllerTest {
         private static final String KEYWORD_SET = "this is a large keyword set";
@@ -75,5 +73,37 @@ class ArticlesControllerTest {
                                 .andExpect(jsonPath("$.response[0].keywords", Matchers.equalTo(KEYWORD_SET)))
                                 .andExpect(jsonPath("$.response[0].title", Matchers.equalTo(title)));
         }
-}
 
+        @Test
+        void testGetArticleById() throws Exception {
+
+                Integer articleId = 1;
+                Integer eventCategory = 45;
+                String title = "Article " + 1;
+                String author = "Author " + 1;
+                String content = "Content " + 1;
+
+                var article = new Article();
+                article.setArticleId(articleId);
+                article.setTitle(title);
+                article.setAuthor(author);
+                article.setAddedDateTime(LocalDateTime.now());
+                article.setEventCategory(eventCategory);
+                article.setKeywords(KEYWORD_SET);
+                article.setContent(content);
+                when(articleService.getArticleById(1)).thenReturn(article);
+
+                mvc.perform(MockMvcRequestBuilders.get("/api/article/1"))
+                                .andExpect(status().isOk())
+                                .andExpect(jsonPath("$.ok", Matchers.equalTo(true)))
+                                .andExpect(jsonPath("$.response", Matchers.notNullValue()))
+
+                                .andExpect(jsonPath("$.response.articleId", Matchers.equalTo(articleId)))
+                                .andExpect(jsonPath("$.response.eventCategory", Matchers.equalTo(eventCategory)))
+                                .andExpect(jsonPath("$.response.author", Matchers.equalTo(author)))
+                                .andExpect(jsonPath("$.response.addedDateTime", Matchers.notNullValue()))
+                                .andExpect(jsonPath("$.response.content", Matchers.equalTo(content)))
+                                .andExpect(jsonPath("$.response.keywords", Matchers.equalTo(KEYWORD_SET)))
+                                .andExpect(jsonPath("$.response.title", Matchers.equalTo(title)));
+        }
+}
