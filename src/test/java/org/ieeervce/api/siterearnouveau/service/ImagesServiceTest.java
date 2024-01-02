09e32e308d77ffd1b88c8e93c27b98ad09d7478c
Atreya Bain <atreyabain@gmail.com>
2024-01-02 12:38:40 +0530

    [test] add images [fix] fix incorrect autowire

--- src/test/java/org/ieeervce/api/siterearnouveau/service/ImagesServiceTest.java
+++ src/test/java/org/ieeervce/api/siterearnouveau/service/ImagesServiceTest.java
@@ -1,10 +1,11 @@
 package org.ieeervce.api.siterearnouveau.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import java.util.Collections;
+import java.util.Optional;
 
-import org.assertj.core.api.Assertions;
 import org.ieeervce.api.siterearnouveau.entity.Image;
 import org.ieeervce.api.siterearnouveau.repository.ImagesRepository;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class ImagesServiceTest {
+    private static final byte[] IMAGE_BYTES = new byte[] { 1, 2 };
+
+    private static final Integer IMAGE_ID = 1;
+
     @Mock
     ImagesRepository imageRepository;
 
@@ -22,13 +27,29 @@ class ImagesServiceTest {
     ImageService imageService;
 
     @Mock
-    Image image1;
-
+    Image image;
 
     @Test
-    void testList(){
-        when(imageRepository.findAll()).thenReturn(Collections.singletonList(image1));
+    void testList() {
+        when(imageRepository.findAll()).thenReturn(Collections.singletonList(image));
         var imageList = imageService.list();
-        Assertions.assertThat(imageList).isNotNull().isNotEmpty().hasSize(1);
+        assertThat(imageList).isNotNull().isNotEmpty().hasSize(1);
+    }
+
+    @Test
+    void testGetBytes() {
+        when(imageRepository.findById(IMAGE_ID)).thenReturn(Optional.of(image));
+        when(image.getImageBytes()).thenReturn(IMAGE_BYTES);
+        Optional<byte[]> imageBytesReturned = imageService.getBytesByImageId(IMAGE_ID);
+
+        assertThat(imageBytesReturned).hasValue(IMAGE_BYTES);
+    }
+
+    @Test
+    void testGetBytesEmpty() {
+        when(imageRepository.findById(IMAGE_ID)).thenReturn(Optional.empty());
+        Optional<byte[]> imageBytesReturned = imageService.getBytesByImageId(IMAGE_ID);
+
+        assertThat(imageBytesReturned).isEmpty();
     }
 }
