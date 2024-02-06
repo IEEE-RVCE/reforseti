97c1b70773f74b5e6605f8f782d7b0b3fce1829e
Atreya Bain <atreyabain@gmail.com>
2024-02-06 18:06:32 +0530

    JWT

--- src/main/java/org/ieeervce/api/siterearnouveau/config/SecurityConfig.java
+++ src/main/java/org/ieeervce/api/siterearnouveau/config/SecurityConfig.java
@@ -1,20 +1,26 @@
 package org.ieeervce.api.siterearnouveau.config;
 
+import org.ieeervce.api.siterearnouveau.auth.jwt.JWTAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+
     /**
      * Password encoder to use for decoding and encoding passwords.
      * <p>
@@ -28,24 +34,28 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    public SecurityFilterChain securityFilterChain(JWTAuthenticationFilter jwtAuthenticationFilter,HttpSecurity httpSecurity) throws Exception {
         // FIXME re-enable cors
-        httpSecurity.csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.disable())
-                .authorizeHttpRequests(getHttpAuthorizationCustomizer())
-                .sessionManagement(
-                    sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(SecurityConfig::getCustomizedHttpAuthorization)
+                .sessionManagement(SecurityConfig::customizeSessionManagement)
+                .addFilterBefore(jwtAuthenticationFilter,UsernamePasswordAuthenticationFilter.class)
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
     }
 
-    private Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> getHttpAuthorizationCustomizer() {
-        return customizer -> customizer
+    private static void getCustomizedHttpAuthorization(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry customizer) {
+        customizer
                 .requestMatchers(HttpMethod.POST, "/api/**").authenticated()
                 .requestMatchers(HttpMethod.DELETE, "/api/**").authenticated()
                 .anyRequest().permitAll();
+    }
+
+
+    private static void customizeSessionManagement(SessionManagementConfigurer<HttpSecurity> sessionManagement) {
+        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
 }
