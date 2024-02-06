c0eb8a3028c4cb7cda81a4c8057475be0764da64
Atreya Bain <atreyabain@gmail.com>
2024-02-06 18:26:35 +0530

    [clean] imports

--- src/main/java/org/ieeervce/api/siterearnouveau/auth/jwt/JWTAuthenticationFilter.java
+++ src/main/java/org/ieeervce/api/siterearnouveau/auth/jwt/JWTAuthenticationFilter.java
@@ -1,26 +1,21 @@
 package org.ieeervce.api.siterearnouveau.auth.jwt;
 
-import java.io.IOException;
-import java.util.Optional;
-
-import lombok.AllArgsConstructor;
-import org.ieeervce.api.siterearnouveau.auth.AuthUserDetails;
-import org.ieeervce.api.siterearnouveau.service.AuthUserDetailsService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.ieeervce.api.siterearnouveau.auth.AuthUserDetails;
+import org.ieeervce.api.siterearnouveau.service.AuthUserDetailsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
 
 /**
  * Once per filter to add jwt authentication details
