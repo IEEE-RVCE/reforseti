25fa738c830248a8b6b1c888e885f96b2db3fab2
Prajwal P <prajwalprakash3722@gmail.com>
2024-02-12 19:53:18 +0530

    [feat] Added Page Support

--- src/main/java/org/ieeervce/api/siterearnouveau/controller/EventsController.java
+++ src/main/java/org/ieeervce/api/siterearnouveau/controller/EventsController.java
@@ -1,10 +1,10 @@
 package org.ieeervce.api.siterearnouveau.controller;
 
-import java.util.List;
-
 import org.ieeervce.api.siterearnouveau.dto.ResultsDTO;
 import org.ieeervce.api.siterearnouveau.entity.Event;
 import org.ieeervce.api.siterearnouveau.service.EventsService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/event")
 public class EventsController {
-    private EventsService eventsService;
+    private final EventsService eventsService;
 
     public EventsController(EventsService eventsService) {
         this.eventsService = eventsService;
     }
 
     @GetMapping
-    public ResultsDTO<List<Event>> list() {
-        List<Event> events = eventsService.list();
+    public ResultsDTO<Page<Event>> list(Pageable pageable) {
+        Page<Event> events = eventsService.list(pageable);
         return new ResultsDTO<>(events);
     }
 }
