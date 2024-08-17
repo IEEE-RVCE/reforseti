458a1693318cfc92821e4c8a0bf8ed1395fd03cd
Atreya Bain <atreyabain@gmail.com>
2024-08-17 11:21:27 +0530

    [cln] add debug log

--- src/main/java/org/ieeervce/api/siterearnouveau/dto/auth/UsernamePasswordDTO.java
+++ src/main/java/org/ieeervce/api/siterearnouveau/dto/auth/UsernamePasswordDTO.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UsernamePasswordDTO {
     @NotBlank
-    @Size(min = 6, max = 10)
+    @Size(min = 6)
     private String userId;
     @NotBlank
     @Size(min = 6)
